eb43d460713096baa5cc3760e346f19ef5a95a8c
shahin <sarker4292@gmail.com>
2026-02-26 06:30:02 +0600

    signUp complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,10 @@ import 'auth/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
 
